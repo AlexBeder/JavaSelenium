d77434fd81d8f8cb9b9c93f9b7402148ad0f7ff4
Ruilcaus <beder.alex@gmail.com>
2021-12-12 22:07:34 -0500

    Edge driver

--- src/test/java/simpleTest/DemoQALogin.java
+++ src/test/java/simpleTest/DemoQALogin.java
@@ -1,5 +1,7 @@
 package simpleTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 public class DemoQALogin {
@@ -7,9 +9,19 @@ public class DemoQALogin {
   @Test
   public void loginToApp(){
     // Name = Alex Tester, User = AlexTester, Password = AlexPass1234$
+    // [id='userName']
+    // [id='password']
+    // [id='login']
+    // [id='userName-value']
+
+    System.setProperty("webdriver.edge.driver","msedgedriver.exe");
+    WebDriver driver = new EdgeDriver();
+
+    driver.get("https://demoqa.com/login");
+    driver.findElement().b
 
 
-
+    driver.close();
   }
 
 }
