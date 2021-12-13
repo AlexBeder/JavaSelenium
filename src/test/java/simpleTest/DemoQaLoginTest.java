c37b2d402655d3ff47ce8472725f3b44810b1f2c
Ruilcaus <beder.alex@gmail.com>
2021-12-13 14:53:02 -0500

    Before After annotations

--- src/test/java/simpleTest/DemoQaLoginTest.java
+++ src/test/java/simpleTest/DemoQaLoginTest.java
@@ -6,18 +6,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class DemoQALogin {
+public class DemoQaLoginTest {
+  
+  private WebDriver driver;
+
+  @BeforeMethod
+  public void startUp() {
+    // Name = Alex Tester, User = AlexTester, Password = AlexPass1234$
+    System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
+    driver = new EdgeDriver();
+    driver.get("https://demoqa.com/login");
+  }
+
+  @AfterMethod
+  public void tearDown() throws InterruptedException {
+    Thread.sleep(3000);
+    driver.close();
+  }
 
   @Test
   public void loginToApp() throws InterruptedException {
-    // Name = Alex Tester, User = AlexTester, Password = AlexPass1234$
-
-    System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
-    WebDriver driver = new EdgeDriver();
-    driver.get("https://demoqa.com/login");
-
     WebElement userName = driver.findElement(By.cssSelector("[id='userName']"));
     WebElement password = driver.findElement(By.cssSelector("[id='password']"));
     WebElement loginButton = driver.findElement(By.cssSelector("[id='login']"));
@@ -34,9 +46,6 @@ public class DemoQALogin {
       Assert.fail("userName element not found");
     }
 
-
-    Thread.sleep(3000);
-    driver.close();
   }
 
 }
