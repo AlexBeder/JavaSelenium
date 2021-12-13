df017c0cdd3a6724d11130545cbcccec2435c63a
Ruilcaus <beder.alex@gmail.com>
2021-12-13 15:50:39 -0500

    Explicit wait is added

--- src/test/java/simpleTest/DemoQaLoginTest.java
+++ src/test/java/simpleTest/DemoQaLoginTest.java
@@ -5,20 +5,25 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+
 public class DemoQaLoginTest {
 
   private WebDriver driver;
+  private WebDriverWait wait;
 
   @BeforeMethod
   public void startUp() {
     // Name = Alex Tester, User = AlexTester, Password = AlexPass1234$
     System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
     driver = new EdgeDriver();
+    wait = new WebDriverWait(driver,10, 100);
     driver.get("https://demoqa.com/login");
   }
 
@@ -29,7 +34,7 @@ public class DemoQaLoginTest {
   }
 
   @Test
-  public void loginToApp() throws InterruptedException {
+  public void loginToApp() {
     WebElement userName = driver.findElement(By.cssSelector("[id='userName']"));
     WebElement password = driver.findElement(By.cssSelector("[id='password']"));
     WebElement loginButton = driver.findElement(By.cssSelector("[id='login']"));
@@ -37,9 +42,9 @@ public class DemoQaLoginTest {
     userName.sendKeys("AlexTester");
     password.sendKeys("AlexPass1234$");
     loginButton.click();
-    Thread.sleep(2000);
 
     try {
+      wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[id='userName-value']")));
       WebElement enteredUser = driver.findElement(By.cssSelector("[id='userName-value']"));
       Assert.assertEquals(enteredUser.getText(), "AlexTester");
     } catch (NoSuchElementException xx) {
