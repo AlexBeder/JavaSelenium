bdd18ac2bae58913056c467aee697d44bb923694
Ruilcaus <beder.alex@gmail.com>
2021-12-12 20:28:17 -0500

    Login Test

---
+++ src/test/java/simpleTest/DemoQALogin.java
@@ -0,0 +1,15 @@
+package simpleTest;
+
+import org.testng.annotations.Test;
+
+public class DemoQALogin {
+
+  @Test
+  public void loginToApp(){
+    // Name = Alex Tester, User = AlexTester, Password = AlexPass1234$
+
+
+
+  }
+
+}
