b8984b95b7326615090db797430448b224781137
Ruilcaus <beder.alex@gmail.com>
2021-12-17 11:06:34 -0500

    Optimized imports

--- src/test/java/testCases/LoginPageTest.java
+++ src/test/java/testCases/LoginPageTest.java
@@ -1,10 +1,6 @@
 package testCases;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 import pages.ProfilePage;
