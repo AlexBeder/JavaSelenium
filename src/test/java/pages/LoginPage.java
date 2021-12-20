0824459c25d36c2d5a970b66526db7b2f0a553bf
Ruilcaus <beder.alex@gmail.com>
2021-12-20 15:04:49 -0500

    Added log4j2.xml + some loggers

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -7,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoginPage extends BasePage{
-
+  private static Logger logger = LogManager.getLogger(LoginPage.class);
 
   public LoginPage(WebDriver driver) {
     super(driver);
@@ -30,9 +32,11 @@ public class LoginPage extends BasePage{
     return driver.findElement(By.cssSelector("[id='login']"));
   }
   public ProfilePage login(String userName, String password) {
+    logger.info("In the login method");
     getUserNameField().sendKeys(userName);
     getPasswordField().sendKeys(password);
     getLoginButton().click();
+    logger.info("Login button is clicked");
     return new ProfilePage(driver);
   }
 
